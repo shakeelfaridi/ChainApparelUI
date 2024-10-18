b3671cbdc94c836fda98737bf5fd8360d60e725e
Muhammad Ahmad <72911692+m-ahmad001@users.noreply.github.com>
2024-10-18 10:53:36 +0000

    update role

--- src/layouts/dashboard/navbar/NavbarAccount.js
+++ src/layouts/dashboard/navbar/NavbarAccount.js
@@ -58,7 +58,7 @@ export default function NavbarAccount({ isCollapse }) {
             {user?.name}
           </Typography>
           <Typography variant="body2" noWrap sx={{ color: "text.secondary" }}>
-            {user?.role}
+            {user?.role === "vendor" ? "supplier" : user?.role }
           </Typography>
         </Box>
       </RootStyle>
